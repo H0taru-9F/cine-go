dbd59567c4b4def17266214f2b0629d048e3ce07
Vasyl <feniak2003@gmail.com>
2025-05-29 15:15:10 +0300

    Add API modules, global styles, and auth templates
    This commit introduces several API modules for movies,

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Routes config
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "@/routes";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+//Datepicker
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterMoment } from "@mui/x-date-pickers/AdapterMoment";
 
-export default App
+// Components
+import PageLoader from "./components/PageLoader";
+
+const App = () => (
+    <LocalizationProvider dateAdapter={AdapterMoment}>
+        <Suspense fallback={<PageLoader />}>
+            <Router>
+                <Routes />
+            </Router>
+        </Suspense>
+    </LocalizationProvider>
+);
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// i18next translation
+import "./i18n";
+
+// Redux config
+import { Provider } from "react-redux";
+import configureStore from "@/store";
+
+// Components
+import App from "@/App";
+import GlobalStyles from "@/components/GlobalStyles";
+
+const store = configureStore();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Provider store={store}>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
+    </Provider>,
+);
